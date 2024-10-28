301528c5050961a3a97ba84785113e6333858cc9
atharv goyal <atharv.goyoal506@gmail.com>
2024-10-29 00:53:54 +0530

    user welcome page

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,5 +4,6 @@ from mainapp import views
 
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('',views.index, name='index'),
+    path('login',views.login,name='login'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -6,9 +6,11 @@ from django.http import HttpResponse
 def index(request):
     return render(request,"index.html")
 
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['Username']
 
 
-
-
-
+ 
+    return render(request,"welcome.html",{'username':username})
 
